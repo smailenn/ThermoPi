d53e45a607b58435eef1b85c8b0fb1d9d046cae3
Mailman <63414848+smailenn@users.noreply.github.com>
2022-03-04 22:42:32 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import glob
+import time
+
+MBR = "Master Bedroom"
+MF = "Main Floor"
+UF = "Upper Floor"
+
+base_dir = '/sys/bus/w1/devices/'
+MBR_folder = glob.glob(base_dir + '28-00000de8f995')[0]
+MF_folder = glob.glob(base_dir + '28-00000de9525b')[0]
+UF_folder = glob.glob(base_dir + '28-00000dea8b78')[0]
+MBR_file = MBR_folder + '/w1_slave'
+MF_file = MF_folder + '/w1_slave'
+UF_file = UF_folder + '/w1_slave'
+
+def read_temp(sensor):
+    f = open(sensor, 'r')
+    lines = f.readlines()
+    f.close()
+    #lines = read_temp_raw(MBR_file)
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_f
+
+while True:
+    print("Master = ", read_temp(MBR_file), "Main Floor =", read_temp(MF_file), "Upper Floor =", read_temp(UF_file))
+    #print(read_temp(MF_file))
+    #print(read_temp(UF_file))
+    time.sleep(1)
