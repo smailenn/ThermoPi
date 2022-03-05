76d41b736f34aa3375cbddc9fd815706d679f59e
smailenn <mailensean@gmail.com>
2022-03-04 23:07:56 -0700

    added specs

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import glob
 import time
 
+#Zones
 MBR = "Master Bedroom"
 MF = "Main Floor"
 UF = "Upper Floor"
 
+#Sensor Setup
 base_dir = '/sys/bus/w1/devices/'
 MBR_folder = glob.glob(base_dir + '28-00000de8f995')[0]
 MF_folder = glob.glob(base_dir + '28-00000de9525b')[0]
@@ -12,6 +14,11 @@ UF_folder = glob.glob(base_dir + '28-00000dea8b78')[0]
 MBR_file = MBR_folder + '/w1_slave'
 MF_file = MF_folder + '/w1_slave'
 UF_file = UF_folder + '/w1_slave'
+
+# Specifications
+Temp_high = 71 # Deg F
+Temp_low = 68
+Run_time = 20 #Minutes
 
 def read_temp(sensor):
     f = open(sensor, 'r')
