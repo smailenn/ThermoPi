a130ab7c768f9ae15caa7273949c2523e6859df1
sean <mailensean@gmail.com>
2022-08-14 16:21:44 -0600

    have it working well now

--- main.py
+++ main.py
@@ -40,12 +40,14 @@ Zone = [MBR_file, MF_file, UF_file]
 # Specifications/ Temps
 Zone_high = [73, 73, 74]  #MAX temperature of zone
 Zone_low = [68, 69, 70]  #MIN temperature of zone
-Zone_time = [1200, 1800, 1800] #seconds of time have per zone will run
+Zone_time = [900, 900, 900] #seconds of time have per zone will run
 Zone_Status = [0, 0, 0] #Status of heating, 0 = not on, 1 = already on
 Zone_On_Time = [0,0,0]  # What time did the zone turn on?
 Zone_Off_Time = [0,0,0] # What time did the zone turn off?
-Zone_Switch = [1, 1, 1] # Is this have able if needed? 0 = no, 1 = yes
+Zone_Switch = [1, 1, 1] # Is this unit turned on or off? 0 = off, 1 = on
+Zone_available = [1,1,1] #Is this unit available or resting?  0 = no, 1 = yes
 Zone_Wait = 300 # dwell time between run times
+Temp_dwell = 1 #time between measurements 
 
 #Relay setup
 Relay = [25, 12, 20]
@@ -60,28 +62,43 @@ GPIO.output(Relay[2], GPIO.LOW) # out
 
 # Function for reading DS18B20 Sensors, get values
 def read_temp(sensor):
-    f = open(sensor, 'r')
-    lines = f.readlines()
-    #print(lines)
-    f.close()
-
-    if lines[0].strip()[-3:] == 'YES':
-        #print("good data")
+    while True: 
+        f = open(sensor, 'r')
+        lines = f.readlines()
+        #print(lines)
+        f.close()
         t_value = lines[1].find('t=')
-        if t_value != -1:
-            temp_string = lines[1][t_value+2:]
-            print(temp_string)
-            temp_c = float(temp_string) / 1000.0
-            temp_f = temp_c * 9.0 / 5.0 + 32.0
-            #print(temp_f)
-            round(temp_f, 3)
-            return temp_f
-        else:
-            print("bad temp data")
-            read_temp(sensor)
-    else:
-        print("Sensor says no")
-        read_temp(sensor)
+        temp_string = lines[1][t_value+2:]
+        #print(temp_string)
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        #print(temp_f)
+        round(temp_f, 3)
+        return temp_f
+        break
+
+    # f = open(sensor, 'r')
+    # lines = f.readlines()
+    # #print(lines)
+    # f.close()
+
+    # if lines[0].strip()[-3:] == 'YES':
+    #     #print("good data")
+    #     t_value = lines[1].find('t=')
+    #     if t_value != -1:
+    #         temp_string = lines[1][t_value+2:]
+    #         print(temp_string)
+    #         temp_c = float(temp_string) / 1000.0
+    #         temp_f = temp_c * 9.0 / 5.0 + 32.0
+    #         #print(temp_f)
+    #         round(temp_f, 3)
+    #         return temp_f
+    #     else:
+    #         print("bad temp data")
+    #         read_temp(sensor)
+    # else:
+    #     print("Sensor says no")
+    #     read_temp(sensor)
 
 # Function for turning on or off heat
 def hvac(sensor,i):
@@ -90,42 +107,38 @@ def hvac(sensor,i):
         GPIO.output(Relay[i], GPIO.HIGH)
         Zone_Status[i] = 1
         Zone_On_Time[i] = time.monotonic()
-        print("Heat Turned on for =", Zone_Name[i])
-        print (time.ctime())
+        print("Heat Turned on for", Zone_Name[i])
+        #print (time.ctime())
 
-    # Turn off heating if conditions are met
+    # Turn off heating if temperature range has been achieved 
     if read_temp(sensor) >= Zone_high[i] and Zone_Status[i] == 1:
         GPIO.output(Relay[i], GPIO.LOW)
         Zone_Status[i] = 0
         Zone_Off_Time[i] = time.monotonic()
-        print("Heat Turned off for =", Zone_Name[i])
-        print (time.ctime())
-''''
-    # Turn off heating if run time is greater than desired
+        print("Heat Turned off for", Zone_Name[i],"temperature achieved")
+        #print (time.ctime())
+
+    #Turn off heating if run time is greater than desired
     if (time.monotonic() - Zone_On_Time[i]) >= Zone_time[i] :
         GPIO.output(Relay[i], GPIO.LOW)
         Zone_Status[i] = 0
         Zone_Off_Time[i] = time.monotonic()
-        print("Heat Turned off for =", Zone_Name[i])
-        print("Heat turned off for being greater than run time desired")
-        print (time.ctime())'''
+        #Zone_Switch[i] = 0  #make the unit unavailable 
+        print("Heat Turned off for", Zone_Name[i],"for being greater than run time desired")
+        #print (time.ctime())
 
-    #else:
-        #print("Nothing to talk about")
-        #print(Zone)
-
-### The Loop
+####################### The Loop  ################################
 while True:
-    # Read temps from sensors
+    # Read temps from sensors and turn on heat if needed and wanted 
     for i in range(3):
         read_temp(Zone[i])
         hvac(Zone[i],i)
 
+    print(time.ctime())
+    for i in range(3):
+        print(Zone_Name[i],":", read_temp(Zone[i]),"F   Zone Status:", Zone_Status[i])
+    print()
+    time.sleep(Temp_dwell)
 
-    #print(time.ctime(), Zone_Name[0], read_temp(Zone[0]), Zone_Name[1], read_temp(Zone[1]), Zone_Name[2], read_temp(Zone[2]))
-    #print(time.time())
-
-    time.sleep(1)
-    #print(time.monotonic())
 
 
