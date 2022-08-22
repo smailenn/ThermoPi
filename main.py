77ac39c6abe8d0388b7811b131fe07bb7bdd9da8
sean <mailensean@gmail.com>
2022-08-21 22:37:18 -0600

    added try

--- main.py
+++ main.py
@@ -14,14 +14,23 @@ import time
 import RPi.GPIO as GPIO
 import os
 import subprocess
+import csv
+import matplotlib.pyplot as plt
 #import /home/pi/ThermoPi/homekit.py
-#from subprocess import*
 
+#Homekit setup 
+#from subprocess import*
 #subprocess.call("/home/pi/ThermoPi/homekit", shell=True)
 #p = open([r'/home/pi/ThermoPi/homekit.py', "ArcView"])
 #output = p.communicate()
 #print(output[0])
 
+#Plot data live
+plt.ion()
+x = []
+y = []
+
+# Pi board setup
 GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
 
 #Sensor Setup
@@ -40,14 +49,14 @@ Zone = [MBR_file, MF_file, UF_file]
 # Specifications/ Temps
 Zone_high = [73, 73, 74]  #MAX temperature of zone
 Zone_low = [68, 69, 70]  #MIN temperature of zone
-Zone_time = [900, 900, 900] #seconds of time have per zone will run
+Zone_time = [40, 20, 9] #seconds of time have per zone will run
 Zone_Status = [0, 0, 0] #Status of heating, 0 = not on, 1 = already on
 Zone_On_Time = [0,0,0]  # What time did the zone turn on?
 Zone_Off_Time = [0,0,0] # What time did the zone turn off?
 Zone_Switch = [1, 1, 1] # Is this unit turned on or off? 0 = off, 1 = on
 Zone_available = [1,1,1] #Is this unit available or resting?  0 = no, 1 = yes
-Zone_Wait = 300 # dwell time between run times
-Temp_dwell = 1 #time between measurements 
+Zone_Wait = 30 # dwell time between run times
+Temp_dwell = 15 #time between measurements 
 
 #Relay setup
 Relay = [25, 12, 20]
@@ -67,38 +76,19 @@ def read_temp(sensor):
         lines = f.readlines()
         #print(lines)
         f.close()
-        t_value = lines[1].find('t=')
+        try:
+            t_value = lines[1].find('t=')
+        except:
+            read_temp(sensor)
         temp_string = lines[1][t_value+2:]
         #print(temp_string)
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
+        #temp_f = 55
         #print(temp_f)
         round(temp_f, 3)
         return temp_f
         break
-
-    # f = open(sensor, 'r')
-    # lines = f.readlines()
-    # #print(lines)
-    # f.close()
-
-    # if lines[0].strip()[-3:] == 'YES':
-    #     #print("good data")
-    #     t_value = lines[1].find('t=')
-    #     if t_value != -1:
-    #         temp_string = lines[1][t_value+2:]
-    #         print(temp_string)
-    #         temp_c = float(temp_string) / 1000.0
-    #         temp_f = temp_c * 9.0 / 5.0 + 32.0
-    #         #print(temp_f)
-    #         round(temp_f, 3)
-    #         return temp_f
-    #     else:
-    #         print("bad temp data")
-    #         read_temp(sensor)
-    # else:
-    #     print("Sensor says no")
-    #     read_temp(sensor)
 
 # Function for turning on or off heat
 def hvac(sensor,i):
@@ -107,7 +97,7 @@ def hvac(sensor,i):
         GPIO.output(Relay[i], GPIO.HIGH)
         Zone_Status[i] = 1
         Zone_On_Time[i] = time.monotonic()
-        print("Heat Turned on for", Zone_Name[i])
+        print("Heat Turned on for", Zone_Name[i],"at",Zone_On_Time[i])
         #print (time.ctime())
 
     # Turn off heating if temperature range has been achieved 
@@ -115,30 +105,56 @@ def hvac(sensor,i):
         GPIO.output(Relay[i], GPIO.LOW)
         Zone_Status[i] = 0
         Zone_Off_Time[i] = time.monotonic()
-        print("Heat Turned off for", Zone_Name[i],"temperature achieved")
+        print("Heat Turned off for", Zone_Name[i],"temperature achieved","at",Zone_Off_Time[i])
         #print (time.ctime())
 
     #Turn off heating if run time is greater than desired
-    if (time.monotonic() - Zone_On_Time[i]) >= Zone_time[i] :
+    if Zone_Status[i] == 1 and (time.monotonic() - Zone_On_Time[i]) >= Zone_time[i]:
         GPIO.output(Relay[i], GPIO.LOW)
         Zone_Status[i] = 0
         Zone_Off_Time[i] = time.monotonic()
         #Zone_Switch[i] = 0  #make the unit unavailable 
-        print("Heat Turned off for", Zone_Name[i],"for being greater than run time desired")
+        print("Heat Turned off for", Zone_Name[i],"for being greater than run time desired at",Zone_Off_Time[i])
         #print (time.ctime())
 
-####################### The Loop  ################################
-while True:
-    # Read temps from sensors and turn on heat if needed and wanted 
-    for i in range(3):
-        read_temp(Zone[i])
-        hvac(Zone[i],i)
+    #Make unit available again 
+    if (time.monotonic() - Zone_Off_Time[i]) >= Zone_Wait:
+        Zone_available[i] = 1
 
-    print(time.ctime())
-    for i in range(3):
-        print(Zone_Name[i],":", read_temp(Zone[i]),"F   Zone Status:", Zone_Status[i])
-    print()
-    time.sleep(Temp_dwell)
+####################### The Loop  ################################
+# Create CSV to log ThermoPi Data
+header = ['Time',"Zone Name","Zone Temp (f)","Status","Zone Name","Zone Temp (f)","Status","Zone Name","Zone Temp (f)","Status"]
+with open("/home/pi/ThermoPi/ThermoPi2022.csv","w") as log:
+    writer = csv.writer(log)
+    writer.writerow(header)
+
+    while True:
+        # Read temps from sensors and turn on heat if needed and wanted 
+        for i in range(3):
+            read_temp(Zone[i])
+            hvac(Zone[i],i)
+
+        #Serial print for debug
+        print(time.ctime())
+        for i in range(3):
+            print(Zone_Name[i],":", read_temp(Zone[i]),"F   Zone Status:", Zone_Status[i])
+        print()
+
+        #Mat plot for live viewing
+        y.append(read_temp(Zone[0]))
+        x.append(time.time())
+        plt.clf()
+        plt.scatter(x,y)
+        plt.plot(x,y)
+        plt.pause(1)
+        plt.draw()
+
+        #Saving data to file or database
+        data = [time.ctime(),Zone_Name[0],read_temp(Zone[0]),Zone_Status[0],Zone_Name[1],read_temp(Zone[1]),Zone_Status[1],Zone_Name[2],read_temp(Zone[2]),Zone_Status[2]]
+        writer.writerow(data)
+        log.flush()
+
+        time.sleep(Temp_dwell)
 
 
 
